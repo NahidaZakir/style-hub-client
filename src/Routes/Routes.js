489642f59f52bfea022489bfce22ce81dc73eabd
Nahida <nahidazakir15@gmail.com>
2022-11-25 22:24:46 +0600

    login signup page complete

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,6 +1,11 @@
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
+import Login from "../Pages/Login/Login";
 import Products from "../Pages/Products/Products";
+import SignUp from "../Pages/SignUp/SignUp";
+
+
+
 
 const { createBrowserRouter } = require("react-router-dom");
 
@@ -17,7 +22,16 @@ const router = createBrowserRouter([
                 path: '/category/:id',
                 element: <Products></Products>,
                 loader: ({ params }) => fetch(`http://localhost:5000/category/${params.id}`)
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
             }
+
         ]
     }
 ])
